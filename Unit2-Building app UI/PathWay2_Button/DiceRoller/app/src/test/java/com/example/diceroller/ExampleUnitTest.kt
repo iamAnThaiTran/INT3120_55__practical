5b0366ad485e9055bc287b5e34b2429c10bc564f
anthaitran1001@gmail.com <anthaitran1001@gmail.com>
2024-09-25 03:09:26 +0700

    path1 unit3

--- Unit2-Building app UI/PathWay2_Button/DiceRoller/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ Unit2-Building app UI/PathWay2_Button/DiceRoller/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,17 +1,43 @@
 package com.example.diceroller
 
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
+ *
  */
+class Square (var dai: Int, var rong: Int) {
+    fun funtion2Test(): Int {
+        return dai - rong
+    }
+
+    fun setVal(a: Int, b: Int) {
+        dai = a
+        rong = b
+    }
+}
 class ExampleUnitTest {
+
+    companion object {
+        private lateinit var exampleSquare: Square
+        @Before
+        fun setUp() {
+            exampleSquare = Square(20, 30)
+        }
+
+        @After
+        fun clear() {
+            exampleSquare.setVal(60, 50)
+        }
+    }
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun daiLonHonRong() {
+        assertTrue(exampleSquare.funtion2Test() > 0)
     }
 }
